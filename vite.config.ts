93cf9b3bb77ed21826af4eb598e6dd788a389066
Enes Usta <enesusta@protonmail.com>
2024-11-17 17:52:48 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,52 @@
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { name } from "./package.json";
+
+import dts from "vite-plugin-dts";
+import { defineConfig } from "vite";
+
+const formattedName = name.match(/[^/]+$/)?.[0] ?? name;
+
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react({ include: /\.(js|jsx|ts|tsx)$/ }),
+      dts({
+        insertTypesEntry: true,
+      }),
+    ],
+    server: {
+      open: true, // If we want to open the app once its started
+    },
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, "src/index.ts"),
+        name: formattedName,
+        formats: ["es", "umd"],
+        fileName: (format) => `${formattedName}.${format}.js`,
+      },
+      rollupOptions: {
+        external: [
+          "react",
+          "react/jsx-runtime",
+          "react-dom",
+          "@mantine/core",
+          "@mantine/dates",
+          "@mantine/dropzone",
+          "@mantine/form",
+          "@mantine/hooks",
+          "@mantine/modals",
+          "@mantine/nprogress",
+          "mantine-datatable",
+        ],
+        output: {
+          globals: {
+            react: "React",
+            "react/jsx-runtime": "react/jsx-runtime",
+            "react-dom": "ReactDOM",
+          },
+        },
+      },
+    },
+  };
+});
